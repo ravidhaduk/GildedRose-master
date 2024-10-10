61a0fc881c9c49f36b7e98eb664c7a629e4b6d58
Ravi Dhaduk <Ravi.Dhaduk@hays.com.au>
2024-10-10 17:15:04 +1100

    Utilizing JSON as source of truth

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using GildedRose.Console.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace GildedRose.Console
 {
@@ -9,25 +12,12 @@ namespace GildedRose.Console
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
+            string filePath = "Items.json";
+            System.Console.WriteLine("Current Directory: " + Directory.GetCurrentDirectory());
 
-                          };
+            IList<Item> Items = LoadItems(filePath);
 
+            var app = new GildedRose(Items);
             app.UpdateQuality();
 
             System.Console.ReadKey();
@@ -110,15 +100,15 @@ namespace GildedRose.Console
             }
         }
 
+        private static IList<Item> LoadItems(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"The file {filePath} does not exist.");
+            }
+
+            string jsonData = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Item>>(jsonData);
+        }
     }
-
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
-
 }
